a07daa369a9125ce707d9f65b915d1ee11b10244
basil1112 <basil1112@gmail.com>
2023-09-18 16:02:51 +0530

    warning msg and inline height increased

--- src/components/common/DimensionPopUp.tsx
+++ src/components/common/DimensionPopUp.tsx
@@ -14,14 +14,28 @@ interface DimensionPopUpProps {
 const DimensionPopUp: React.FC<DimensionPopUpProps> = ({ isRectDialogVisible, userWidth, userHeight, setUserRectWidth, setUserRectHeight, validateDimension, setRectDialogVisible }) => {
 
     return (
-        <Dialog header="Box Dimensions" visible={isRectDialogVisible} style={{ width: '50vw' }} onHide={() => setRectDialogVisible(false)}>
+        <Dialog header="Box Dimensions" visible={isRectDialogVisible} style={{ width: '30vw' }} onHide={() => setRectDialogVisible(false)}>
             <div className='container'>
                 <div className='row line_spacing'>
-                    <table>
+                    <table className="dimension">
                         <tbody>
                             <tr>
-                                <td><input type='number' className='form-control' value={userWidth} placeholder='Enter Box Width' onChange={(e) => { setUserRectWidth(e.target.value) }} /></td>
-                                <td><input type='number' className='form-control' value={userHeight} placeholder='Enter Box Height' onChange={(e) => { setUserRectHeight(e.target.value) }} /></td>
+                                <td>
+                                    <label>Width</label>
+                                    <input type='number' className='form-control' value={userWidth} autoFocus={true} placeholder='Enter Box Width' onChange={(e) => { setUserRectWidth(e.target.value) }} />
+                                </td>
+                            </tr>
+                            <tr>
+
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label>Height</label>
+                                    <input type='number' className='form-control' value={userHeight} placeholder='Enter Box Height' onChange={(e) => { setUserRectHeight(e.target.value) }} />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> <span className="text-warning"> * All values between 10 - 70</span></td>
                             </tr>
                         </tbody>
                     </table>
